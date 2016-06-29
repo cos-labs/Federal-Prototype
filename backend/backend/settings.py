63c1b12fa310c8082b2f3d004ab42ac6198cc117
Cameron Blandford <blandford@virginia.edu>
2016-06-29 10:25:35 -0400

    Update database settings for postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "fed",  # database name
+        "USER": "admin",  # user name
+        "PASSWORD": "password",  # user password
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
