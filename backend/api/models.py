2b60aa1a2647f7cbb9a0790f1d07ece721de2763
Cameron Blandford <blandford@virginia.edu>
2016-06-30 14:45:24 -0400

    Make backend pep8-compliant

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,32 +1,33 @@
 from __future__ import unicode_literals
+
 from django.db import models
 
 
-
 class Document(models.Model):
-	date_submitted = models.DateTimeField(auto_now_add=True)
-	date_published = models.DateTimeField()
-	title = models.CharField(max_length=200)
-	publisher = models.CharField(max_length=100)
-	institution = models.CharField(max_length=100)
-	#departments = models.ManyToManyField(Department),
-	#grant_id = models.ForeignKey('GrantID')
+    date_submitted = models.DateTimeField(auto_now_add=True)
+    date_published = models.DateTimeField()
+    title = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=100)
+    institution = models.CharField(max_length=100)
+    # departments = models.ManyToManyField(Department),
+    # grant_id = models.ForeignKey('GrantID')
 
-	# Need revision, enum for status, something else for file
-	status = models.CharField(max_length=50)
-	file_link = models.CharField(max_length=200)
+    # Need revision, enum for status, something else for file
+    status = models.CharField(max_length=50)
+    file_link = models.CharField(max_length=200)
 
-	# Author information
-	PI_first_name = models.CharField(max_length=50)
-	PI_last_name = models.CharField(max_length=50)
-	PI_email = models.EmailField(max_length=100)
+    # Author information
+    PI_first_name = models.CharField(max_length=50)
+    PI_last_name = models.CharField(max_length=50)
+    PI_email = models.EmailField(max_length=100)
 
-	author_list = models.CharField(max_length=500)
+    author_list = models.CharField(max_length=500)
 
-	class Meta:
-		permissions = (
-			('view_document', 'View document'),
+    class Meta:
+        permissions = (
+            ('view_document', 'View document'),
         )
+
 
 # class Author(models.Model):
 # 	name_first = models.CharField(max_length=50)
@@ -37,16 +38,14 @@ class Document(models.Model):
 
 
 class Department(models.Model):
-	name = models.CharField(max_length=50)
-	documents = models.ManyToManyField(Document)
-	# Admins or Users?
+    name = models.CharField(max_length=50)
+    documents = models.ManyToManyField(Document)
+    # Admins or Users?
 
+    # Possible way to represent grantIDs
+    # class GrantID(models.Model):
+    # 	id = models.CharField(max_length=100)
+    # 	department = models.ForeignKey('Department')
+    #	document = models.ManyToMany('Document')
 
-# Possible way to represent grantIDs
-# class GrantID(models.Model):
-# 	id = models.CharField(max_length=100)
-# 	department = models.ForeignKey('Department')
-#	document = models.ManyToMany('Document')
-
-
-#User and Admin Groups
+    # User and Admin Groups
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
-#removed Author
+# removed Author
 from api.models import Document, Department
 from django.contrib.auth.models import User
 
+
 class DocumentSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Document
-		fields = ('url', 'date_submitted', 'date_published', 'title', 'publisher', 'institution', 
-			'status', 'file_link', 'PI_first_name', 'PI_last_name',
-			'PI_email', 'author_list')
+    class Meta:
+        model = Document
+        fields = ('url', 'date_submitted', 'date_published', 'title', 'publisher', 'institution',
+                  'status', 'file_link', 'PI_first_name', 'PI_last_name',
+                  'PI_email', 'author_list')
+
 
 # class AuthorSerializer(serializers.HyperlinkedModelSerializer):
 # 	documents = serializers.HyperlinkedRelatedField(many=True, view_name='document-detail', read_only=True)
@@ -17,15 +19,17 @@ class DocumentSerializer(serializers.HyperlinkedModelSerializer):
 # 		fields = ('url', 'name_first', 'name_middle', 'name_last', 'email', 'documents')
 
 class DepartmentSerializer(serializers.HyperlinkedModelSerializer):
-	documents = serializers.HyperlinkedRelatedField(many=True, view_name='document-detail', read_only=True)
-	class Meta:
-		model = Department
-		fields = ('url', 'name', 'documents')
+    documents = serializers.HyperlinkedRelatedField(many=True, view_name='document-detail', read_only=True)
+
+    class Meta:
+        model = Department
+        fields = ('url', 'name', 'documents')
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-	# department = serializers.HyperlinkedRelatedField(many=True, view_name='department-detail', read_only=True)
+    # department = serializers.HyperlinkedRelatedField(many=True, view_name='department-detail', read_only=True)
 
-	class Meta:
-		model = User
-		# Eventually include department
-		fields = ('url', 'username')
+    class Meta:
+        model = User
+        # Eventually include department
+        fields = ('url', 'username')
