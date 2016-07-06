83254baaafa58203e421bded2c33637967e648d5
rytiggy <rytiggy@gmail.com>
2016-07-06 16:48:59 -0400

    fixed js-hint

--- ostp_files_emberV2/app/components/department-file-viewer.js
+++ ostp_files_emberV2/app/components/department-file-viewer.js
@@ -4,24 +4,24 @@ export default Ember.Component.extend({
    actions: {
     moreInfo(){
 
-        for(var i = 0; i <= $(".document").length; i++){
-          if($(".more-info-button").index(event.target) == i){
-              $( ".document:eq("+i+")" ).removeClass( "document-view-document" );
-              $( ".document:eq("+i+")" ).toggleClass( "document-more-info" );
-              $( ".view-document:eq("+i+")" ).hide();
-              $( ".more-info:eq("+i+")" ).toggle("fast");
+        for(var i = 0; i <= Ember.$(".document").length; i++){
+          if(Ember.$(".more-info-button").index(event.target) === i){
+              Ember.$( ".document:eq("+i+")" ).removeClass( "document-view-document" );
+              Ember.$( ".document:eq("+i+")" ).toggleClass( "document-more-info" );
+              Ember.$( ".view-document:eq("+i+")" ).hide();
+              Ember.$( ".more-info:eq("+i+")" ).toggle("fast");
             }
         }
 
       },
       viewDocument(){
 
-        for(var i = 0; i <= $(".document").length; i++){
-          if($(".view-document-button").index(event.target) == i){
-            $( ".document:eq("+i+")" ).removeClass( "document-more-info"  );
-            $( ".document:eq("+i+")" ).toggleClass( "document-view-document" );
-            $(".more-info:eq("+i+")").hide();
-            $(".view-document:eq("+i+")").toggle("fast");
+        for(var i = 0; i <= Ember.$(".document").length; i++){
+          if(Ember.$(".view-document-button").index(event.target) === i){
+           Ember.$( ".document:eq("+i+")" ).removeClass( "document-more-info"  );
+           Ember.$( ".document:eq("+i+")" ).toggleClass( "document-view-document" );
+           Ember.$(".more-info:eq("+i+")").hide();
+           Ember.$(".view-document:eq("+i+")").toggle("fast");
             }
         }
 
