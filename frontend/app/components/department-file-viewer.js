7f91630843ada0284d4c44fe51d118a4ea1b8282
Carolyn Stewart <carolynrstewart@gmail.com>
2016-07-08 15:12:37 -0400

    Restructure code; delete unnecessary files

---
+++ frontend/app/components/department-file-viewer.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+   actions: {
+    moreInfo(){
+        for(var i = 0; i <= Ember.$(".document").length; i++){
+          if(Ember.$(".more-info-button").index(event.target) === i){
+           Ember.$( ".document:eq("+i+")" ).removeClass( "document-view-document" );
+           Ember.$( ".document:eq("+i+")" ).toggleClass( "document-more-info" );
+           Ember.$( ".view-document:eq("+i+")" ).hide();
+           Ember.$( ".more-info:eq("+i+")" ).toggle("fast");
+            }
+        }
+      },
+      viewDocument(){
+        for(var i = 0; i <= Ember.$(".document").length; i++){
+          if(Ember.$(".view-document-button").index(event.target) === i){
+           Ember.$( ".document:eq("+i+")" ).removeClass( "document-more-info"  );
+           Ember.$( ".document:eq("+i+")" ).toggleClass( "document-view-document" );
+           Ember.$(".more-info:eq("+i+")").hide();
+           Ember.$(".view-document:eq("+i+")").toggle("fast");
+            }
+        }
+      }
+   }
+});
+
