e3a3ddada519c35ae1ce2eee82a20c1b8847f568
rytiggy <rytiggy@gmail.com>
2016-07-13 09:26:03 -0400

    break up settings page into two parts. Add auto update so users can see the changes they are making

---
+++ frontend/app/components/settings-side-by-side.js
@@ -0,0 +1,122 @@
+import Ember from 'ember';
+//pull from  db
+const schema = {
+  "schema": {
+    "title":"Describe the document",
+    "description":"The meta data associated with the document that was uploaded.",
+    "type":"object",
+    "properties": {
+
+      "document-name": {
+        "type":"string",
+        "title":"Document Name"
+      },
+
+      "date-published": {
+        "type":"string",
+        "title":"Date Published"
+      },
+
+      "publisher": {
+        "type":"string",
+        "title":"Publisher"
+      },
+
+      "institution": {
+        "type":"string",
+        "title":"institution"
+      },
+
+      "pi": {
+        "type":"string",
+        "title":"Principal Investigator"
+      },
+
+      "pi-email": {
+        "type":"string",
+        "title":"Principal Investigator Email"
+      },
+
+      "author-list": {
+        "type":"string",
+        "title":"Author(s)"
+      }
+    }
+  },
+
+
+  "options": {
+    "helper": "The meta data associated with the document that was uploaded.",
+    "fields": {
+
+      "document-name": {
+        "size": 256,
+        "helper": "Please enter the documents name.",
+        "placeholder": "documents name"
+      },
+
+     "date-published": {
+       "format": "date-time",
+        "helper": "Please enter when the document was published.",
+        "placeholder": "e.g. 5/5/1995"
+      },
+
+     "publisher": {
+        "size": 256,
+        "helper": "Please enter the publisher.",
+        "placeholder": "Publisher"
+      },
+
+     "institution": {
+        "size": 256,
+        "helper": "Please enter the institution.",
+        "placeholder": "Institution"
+      },
+
+     "pi": {
+        "size": 156,
+        "helper": "Please enter the Principal Investigator.",
+        "placeholder": "e.g. John Doe"
+      },
+
+     "pi-email": {
+        "format": "email",
+        "size": 256,
+        "helper": "Please enter the Principal Investigator Email address.",
+        "placeholder": "e.g. JohnDoe@mail.com"
+      },
+
+     "author-list": {
+        "size": 256,
+        "helper": "Please enter the auther(s), if you have more than author use commas to separate them.",
+        "placeholder": "e.g. Cameron, Blandford Carolyn Stewart, Ryan Mason"
+      }
+
+    }
+  }
+};
+
+
+export default Ember.Component.extend({
+    _schemaList : [JSON.stringify(schema,null, 4)],
+    metadataInputJson : JSON.stringify(schema,null, 4),
+    schema: Ember.computed('metadataInputJson' , function(){
+         return JSON.parse(this.get("metadataInputJson"));
+      }
+    ),
+    schemaList: Ember.computed('_schemaList.@each', function(){
+        return this.get('_schemaList');
+    }),
+    onSchemaChange: Ember.observer('metadataInputJson', function(){
+         console.log($( "#metadataJson" ));
+
+         $( "#metadataJson" ).focus();
+         this.get("_schemaList").unshiftObject(JSON.parse(this.get("metadataInputJson")));
+    }),
+
+    actions: {
+      save() {
+        console.log("Send to DB");
+      }
+    }
+});
