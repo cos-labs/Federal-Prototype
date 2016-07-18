e03e3e061a183c2448398747d2b236e58531a168
rytiggy <rytiggy@gmail.com>
2016-07-18 15:25:34 -0400

    added front end user feedback change

--- frontend/app/components/status-filter.js
+++ frontend/app/components/status-filter.js
@@ -8,16 +8,24 @@ export default Ember.Component.extend({
    didRender() {
     this._super(...arguments);
      Ember.$('div[ data-item=".read"]').css("border-left", "5px solid #5cb85c");
+     Ember.$('span[ data-filter=".read"] i').css({"color" : "#5cb85c"});
+
      Ember.$('div[ data-item=".unread"]').css("border-left", "5px solid #eea236");
+     Ember.$('span[ data-filter=".unread"] i').css({"color" : "#eea236"});
+
      Ember.$('div[ data-item=".archived"]').css("border-left", "5px solid #337ab7");
+     Ember.$('span[ data-filter=".archived"] i').css({"color" : "#337ab1"});
+
+
   },
   actions: {
     getSelectedStatusFilter() {
       Ember.$(".filter").removeClass("active");
        Ember.$(event.target).addClass( "active" );
-      var sel =  Ember.$(event.target).data('filter');
+       var sel =  Ember.$(event.target).data('filter');
        Ember.$('div.document-list div.document').hide();
-          if(sel !== ".read, .unread, .archived"){
+       console.log(Ember.$(event.target));
+          if(sel !== ".read, .unread, .archived" && sel !== undefined){
               Ember.$('div[ data-item="'+sel+'"]').show();
           }
           else {
