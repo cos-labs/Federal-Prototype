1b0d8c4280f6b4617c0021769c59b6de2d2abfdb
rytiggy <rytiggy@gmail.com>
2016-07-26 16:07:44 -0400

    fixed merge

--- frontend/app/models/document.js
+++ frontend/app/models/document.js
@@ -4,16 +4,6 @@ import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   datesubmitted: attr('string'),
-  datepublished:  attr('string'),
-  title: attr('string'),
-  publisher: attr('string'),
-  institution: attr('string'),
-  status: attr('string'),
-  filelink: attr('string'),
-  pifirstname: attr('string'),
-  pilastname: attr('string'),
-  piemail: attr('string'),
-  authorlist: attr('string'),
   name: attr('string', { defaultValue: 'Untitled'} ),
   path: attr('string'),
   grants: hasMany('grant'),
