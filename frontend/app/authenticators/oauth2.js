bc5395381158e64ee6b72b3c84b9d1b6dd487f7b
Ryan Mason <rmm4514@rit.edu>
2016-09-09 13:48:34 -0400

    fixed error

--- frontend/app/authenticators/oauth2.js
+++ frontend/app/authenticators/oauth2.js
@@ -1,5 +1,5 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: 'http://localhost:8000/api/users?format=api'
+  serverTokenEndpoint: '127.0.0.1:8000'
 });
