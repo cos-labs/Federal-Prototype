a5fa50eed4440bf9481cd116583a12c365d8f214
Ryan Mason <rmm4514@rit.edu>
2016-09-12 09:30:08 -0400

    edited auth url

--- frontend/app/authenticators/oauth2.js
+++ frontend/app/authenticators/oauth2.js
@@ -1,5 +1,5 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: '127.0.0.1:8000'
+  serverTokenEndpoint: "http://127.0.0.1:8000/users/"
 });
