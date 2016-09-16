0a6d68e1acc2a4fe28e4f032f2fee35f6b7d113f
Joshua Bird <admin@Orion.local>
2016-09-16 16:13:44 -0400

    commit

--- frontend/app/routes/researcher.js
+++ frontend/app/routes/researcher.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+ store: Ember.inject.service(),
+ session: Ember.inject.service(),
  beforeModel: function() {
     if(document.location.pathname === "/researcher/grant"){ this.transitionTo('researcher.grant'); }
     else if(document.location.pathname === "/researcher/metadata"){ this.transitionTo('researcher.metadata'); }
--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -20,7 +20,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  app.import('')
   
   return app.toTree();
 };
