f7d9de878e2d5c7ce4fc78ca5090f23a33ef6ae4
Joshua Bird <admin@Orion.local>
2016-09-24 15:06:41 -0400

    change status dropdown to display current status

--- frontend/app/routes/agency/overview.js
+++ frontend/app/routes/agency/overview.js
@@ -4,10 +4,20 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-    
+
     model() {
+        var dept = this.get('store').query('department', {
+            filter: {
+                name: "Department of Open Science",
+            },
+        })
+        this.set('department', dept.get('firstObject'));       
         return Ember.RSVP.hash({
-            grants: this.get('store').findAll('grant'),
+            grants: this.get('store').query('grant', {
+                filter: { 
+                    department: dept.get('firstObject'),
+                },
+            }),
             departments: this.get('store').findAll('department'),
         });
     },
@@ -21,9 +31,7 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     setupController(controller, model) {
         controller.set('institution', true);
         controller.set('isFileUploaded', "researcher-form");
-        controller.set('grants', model.grants.filter(function(grant) {
-            return grant.get('institution');
-        }));
+        controller.set('grants', model.grants);
         //controller.set('document', model.document)
         controller.set('departments', model.departments)
     }
