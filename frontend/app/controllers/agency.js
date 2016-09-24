4fe950b8e83215d3afe89d42a0ee5e4b041c7a2b
Joshua Bird <admin@Orion.local>
2016-09-24 13:58:35 -0400

    fixes and styles and routes and stuff

--- frontend/app/controllers/agency.js
+++ frontend/app/controllers/agency.js
@@ -4,6 +4,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     grants: [],
     agency: true,
+    institution: false,
     //active_grant: null,
     //document: {},
     departments: {},
--- frontend/app/routes/institution.js
+++ frontend/app/routes/institution.js
@@ -4,28 +4,14 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
     
+    store: Ember.inject.service(),
+    session: Ember.inject.service(),
+    
     model() {
         return Ember.RSVP.hash({
             grants: this.get('store').findAll('grant'),
             departments: this.get('store').findAll('department'),
         });
-    },
-    
-    actions: {
-        didTransition: function() {
-            Ember.$(".moderatorHolder").show();
-        }
-    },
-
-    setupController(controller, model) {
-        controller.set('institution', true);
-        controller.set('isFileUploaded', "researcher-form");
-        controller.set('grants', model.grants.filter(function(grant) {
-            return grant.get('institution');
-        }));
-        //controller.set('document', model.document)
-        controller.set('departments', model.departments)
     }
 
 });
-
