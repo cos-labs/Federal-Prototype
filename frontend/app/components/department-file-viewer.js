69fda93e471291f0a71f94e1fcd958c98f09437c
Joshua Bird <admin@Orion.local>
2016-09-25 19:04:01 -0400

    restore working workflow

--- frontend/app/components/department-file-viewer.js
+++ frontend/app/components/department-file-viewer.js
@@ -6,7 +6,9 @@ export default Ember.Component.extend({
     datepublished: '',
     pi: '',
     piemail: '',
-    
+    currentPage: 1,
+    notlastpage: true,
+    notfirstpage: false,  
     didInsertElement(){
         /*var answers = this.grant.get('answers');
         this.set('pi', answers['pi']);
@@ -30,59 +32,63 @@ export default Ember.Component.extend({
 
     },
 
-    afterModel() {
-        this._super(...arguments);
-        this.set('answers', this.grant.get('answers'));
-        this.set('pi', this.get('answers')['pi']);
-    },
+    //afterModel() {
+    //    this._super(...arguments);
+    //    this.set('answers', this.grant.get('answers'));
+    //    this.set('pi', this.get('answers')['pi']);
+    //},
 
     actions: {
         updateStatus(arg) {
             console.log(arg);
         }, 
+        
+        nextPageHook() {
+             //this.attrs.nextPageHook();
+        },
         createGrantHook(dept_id, grant_id, pi_name) {
             this.attrs.createGrantHook(dept_id, grant_id, pi_name);
-        },
-        moreInfo() {
-            for (var i = 0; i <= Ember.$(".document").length; i++) {
-                if (Ember.$(".more-info-button").index(event.target) === i) {
-                    Ember.$(".document:eq("+i+")").removeClass("document-view-document");
-                    Ember.$(".document:eq("+i+")").toggleClass("document-more-info");
-                    Ember.$(".view-document:eq("+i+")").hide();
-                    Ember.$(".more-info:eq("+i+")").toggle("fast");
-                }
-            }
-
-        },
-        viewDocument() {
-            for (var i = 0; i <= Ember.$(".document").length; i++) {
-                if (Ember.$(".view-document-button").index(event.target) === i) {
-                    Ember.$(".document:eq("+i+")").removeClass("document-more-info");
-                    Ember.$(".document:eq("+i+")").toggleClass("document-view-document");
-                    Ember.$(".more-info:eq("+i+")").hide();
-                    Ember.$(".view-document:eq("+i+")").toggle("fast");
-                }
-            }
-        },
-        setDocumentStatus(status , color, buttonType){
-            var thisParents = Ember.$(event.target).parents().eq(6);
-            var thisParentSiblings = Ember.$(event.target).parent().siblings();
-            thisParents.data('item', status);
-            thisParents.attr('data-item', status);
-            thisParents.css("border-left", "5px solid "+color+"");
-            thisParentSiblings.text(Ember.$(event.target).text());
-            thisParentSiblings.removeClass( );
-            thisParentSiblings.toggleClass( buttonType);
-            if (Ember.$(".active").data("filter") !== ".read, .unread, .archived") {
-                if (Ember.$(".active").data("filter") !== status) {
-                    Ember.$(event.target).parents().eq(6).hide("slide");
-                    Ember.$.bootstrapGrowl("Successfully changed document status to " + Ember.$(event.target).text() + "!", { type: 'success', align: 'center' , width: 400, hight: 40 });
-                }
-            } else {
-                Ember.$.bootstrapGrowl("Successfully changed document status to " + Ember.$(event.target).text() + "!", { type: 'success', align: 'center' , width: 400, hight: 40 });
-            }
-            console.log("update database to archive");
         }
+        //moreInfo() {
+        //    for (var i = 0; i <= Ember.$(".document").length; i++) {
+        //        if (Ember.$(".more-info-button").index(event.target) === i) {
+        //            Ember.$(".document:eq("+i+")").removeClass("document-view-document");
+        //            Ember.$(".document:eq("+i+")").toggleClass("document-more-info");
+        //            Ember.$(".view-document:eq("+i+")").hide();
+        //            Ember.$(".more-info:eq("+i+")").toggle("fast");
+        //        }
+        //    }
+
+        //},
+        //viewDocument() {
+        //    for (var i = 0; i <= Ember.$(".document").length; i++) {
+        //        if (Ember.$(".view-document-button").index(event.target) === i) {
+        //            Ember.$(".document:eq("+i+")").removeClass("document-more-info");
+        //            Ember.$(".document:eq("+i+")").toggleClass("document-view-document");
+        //            Ember.$(".more-info:eq("+i+")").hide();
+        //            Ember.$(".view-document:eq("+i+")").toggle("fast");
+        //       }
+        //    }
+        //},
+        //setDocumentStatus(status , color, buttonType){
+        //    var thisParents = Ember.$(event.target).parents().eq(6);
+        //    var thisParentSiblings = Ember.$(event.target).parent().siblings();
+        //    thisParents.data('item', status);
+        //    thisParents.attr('data-item', status);
+        //    thisParents.css("border-left", "5px solid "+color+"");
+        //    thisParentSiblings.text(Ember.$(event.target).text());
+        //    thisParentSiblings.removeClass( );
+        //    thisParentSiblings.toggleClass( buttonType);
+        //    if (Ember.$(".active").data("filter") !== ".read, .unread, .archived") {
+        //        if (Ember.$(".active").data("filter") !== status) {
+        //            Ember.$(event.target).parents().eq(6).hide("slide");
+        //            Ember.$.bootstrapGrowl("Successfully changed document status to " + Ember.$(event.target).text() + "!", { type: 'success', align: 'center' , width: 400, hight: 40 });
+        //        }
+        //    } else {
+        //        Ember.$.bootstrapGrowl("Successfully changed document status to " + Ember.$(event.target).text() + "!", { type: 'success', align: 'center' , width: 400, hight: 40 });
+        //    }
+        //    console.log("update database to archive");
+        //}
 
     }
 });
