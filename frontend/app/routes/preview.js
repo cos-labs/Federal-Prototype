b34882af18b9eb562f89d9346218dede52703584
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-09-27 02:22:03 -0400

    clear out route

--- frontend/app/routes/preview.js
+++ frontend/app/routes/preview.js
@@ -3,31 +3,5 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';4   
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-    model() {
-        return Ember.RSVP.hash({
-            grants: this.get('store').findAll('grant'),
-            documents: this.get('store').findAll('document'),
-            institutions: [
-                'UVA',
-                'HARVARD',
-                'CALTECH',
-                'MIT'
-            ],
-        });
-    },
-    
-    actions: {
-        didTransition: function() {
-            Ember.$(".moderatorHolder").show();
-        }
-    },
-
-    setupController(controller, model) {
-        controller.set('agency', true);
-        controller.set('isFileUploaded', "researcher-form");
-        controller.set('grants', model.grants);
-        controller.set('documents', model.documents);
-        controller.set('institutions', model.institutions);
-    }
 
 });
