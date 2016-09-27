f1de28122f4bafb385c376e686fc248cf2b7d199
Joshua Bird <admin@Orion.local>
2016-09-27 00:21:00 -0400

    cookie auth

--- frontend/app/routes/agency/overview.js
+++ frontend/app/routes/agency/overview.js
@@ -1,9 +1,8 @@
 
 import Ember from 'ember';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend({
 
     model() {
         var dept = this.get('store').query('department', {
