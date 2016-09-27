81bc100444cb331f62ea9d4634c0dd90e14b6c5d
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-09-27 03:33:25 -0400

    resize mfr, styling changes

--- frontend/app/controllers/preview.js
+++ frontend/app/controllers/preview.js
@@ -3,16 +3,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+    filename: undefined,
     path: undefined,
-    back: undefined,
 
     actions: {
-
         fetch(grant) {
-            console.log(grant);
             this.set('path', "https://staging-files.osf.io/v1/resources/s5mvu/providers/osfstorage" + grant.get('document').get('path'));
+            this.set('filename', grant.get('document').get('name'));
         }
-
     }
 
 });        
