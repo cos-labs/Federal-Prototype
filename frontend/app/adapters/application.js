b1a047a0e1e713e3e380e0ca9360153d429ea8cc
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-09-26 22:39:49 -0400

    Update application.js

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,7 +1,7 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 
 export default JSONAPIAdapter.extend({
-  host: '',
+  host: '/api',
   buildURL: function(type, id, record) {
     //call the default buildURL and then append a slash
     // return this._super(type, id, record) + '/';
